ec676fccb24e4bf52c8d3736f197302334548eb3
Juno Nguyen <juno.ngx@gmail.com>
2026-04-11 21:04:51 +0800

    feat: implement local user settings store

---
+++ src/store/localUserSettings.ts
@@ -0,0 +1,13 @@
+import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
+import type { LocalUserSettingsStore } from '../types'
+
+export const useLocalUserSettings = create<LocalUserSettingsStore>()(
+    persist(
+        (set) => ({
+            themeMode: 'system',
+            setThemeMode: (themeMode) => set({ themeMode }),
+        }),
+        { name: 'localUserSettings' },
+    ),
+)
