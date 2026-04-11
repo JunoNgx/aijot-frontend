0e099c3aa1ec70746983d42fedb3a97ddebc67c9
Juno Nguyen <juno.ngx@gmail.com>
2026-04-11 21:12:08 +0800

    feat: implement core collection settings

---
+++ src/store/coreCollectionSettings.ts
@@ -0,0 +1,22 @@
+import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
+import type { CoreCollectionSettingsStore } from '../types'
+import {
+    DEFAULT_ALL_COLLECTION,
+    DEFAULT_UNTAGGED_COLLECTION,
+    DEFAULT_TRASH_COLLECTION,
+} from '../utils/constants'
+
+export const useCoreCollectionSettings = create<CoreCollectionSettingsStore>()(
+    persist(
+        (set) => ({
+            all: DEFAULT_ALL_COLLECTION,
+            untagged: DEFAULT_UNTAGGED_COLLECTION,
+            trash: DEFAULT_TRASH_COLLECTION,
+            setAll: (config) => set((state) => ({ all: { ...state.all, ...config } })),
+            setUntagged: (config) => set((state) => ({ untagged: { ...state.untagged, ...config } })),
+            setTrash: (config) => set((state) => ({ trash: { ...state.trash, ...config } })),
+        }),
+        { name: 'coreCollectionSettings' },
+    ),
+)
