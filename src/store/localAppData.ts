155c1779d8cad5dfd07ad4fbb71c5246eab49a19
Juno Nguyen <juno.ngx@gmail.com>
2026-04-11 21:06:40 +0800

    feat: implement local app data store

---
+++ src/store/localAppData.ts
@@ -0,0 +1,13 @@
+import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
+import type { LocalAppDataStore } from '../types'
+
+export const useLocalAppData = create<LocalAppDataStore>()(
+    persist(
+        (set) => ({
+            shouldShowDemoDataBanner: true,
+            setShouldShowDemoDataBanner: (value) => set({ shouldShowDemoDataBanner: value }),
+        }),
+        { name: 'localAppData' },
+    ),
+)
