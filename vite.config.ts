c6554ac97ec96ec9abd05032f46f49267697bfeb
Juno Nguyen <juno.ngx@gmail.com>
2026-04-12 22:08:20 +0800

    lint: format root files

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
-import { resolve } from 'path'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import { VitePWA } from "vite-plugin-pwa"
+import { resolve } from "path"
 
 export default defineConfig({
     resolve: {
         alias: {
-            '@': resolve(__dirname, 'src'),
+            "@": resolve(__dirname, "src"),
         },
     },
     plugins: [
         react(),
         VitePWA({
-            registerType: 'autoUpdate',
+            registerType: "autoUpdate",
             manifest: {
-                name: 'ai*jot',
-                short_name: 'aijot',
-                start_url: '/jot',
-                display: 'standalone',
-                background_color: '#ffffff',
-                theme_color: '#ffffff',
+                name: "ai*jot",
+                short_name: "aijot",
+                start_url: "/jot",
+                display: "standalone",
+                background_color: "#ffffff",
+                theme_color: "#ffffff",
                 icons: [],
             },
         }),
