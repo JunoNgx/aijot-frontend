93e4a1989b5f1f27b1b66486a7451ae3a7169541
Juno Nguyen <juno.ngx@gmail.com>
2026-04-13 20:01:23 +0800

    feat: implement own useDebounced

---
+++ src/hooks/useDebounced.ts
@@ -0,0 +1,18 @@
+import { useCallback, useRef } from "react"
+
+export function useDebounced<T extends (...args: unknown[]) => void>(
+    callback: T,
+    delay: number,
+): T {
+    const timerRef = useRef<ReturnType<typeof setTimeout> | null>(null)
+
+    return useCallback(
+        (...args: Parameters<T>) => {
+            if (timerRef.current) clearTimeout(timerRef.current)
+            timerRef.current = setTimeout(() => {
+                callback(...args)
+            }, delay)
+        },
+        [callback, delay],
+    ) as T
+}
