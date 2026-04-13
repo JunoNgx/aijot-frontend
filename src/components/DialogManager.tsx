dc8339900f710495a2a9af8e491fd9342738decd
Juno Nguyen <juno.ngx@gmail.com>
2026-04-13 20:07:39 +0800

    feat: implement radix-powered DialogManager

---
+++ src/components/DialogManager.tsx
@@ -0,0 +1,28 @@
+import * as Dialog from "@radix-ui/react-dialog"
+import { useDialogStore } from "@/store/dialogStore"
+import styles from "./DialogManager.module.scss"
+
+export default function DialogManager() {
+    const isOpen = useDialogStore((s) => s.isOpen)
+    const children = useDialogStore((s) => s.children)
+    const closeAllDialogs = useDialogStore((s) => s.closeAllDialogs)
+
+    const handleOpenChange = (open: boolean) => {
+        if (!open) closeAllDialogs()
+    }
+
+    return (
+        <Dialog.Root open={isOpen} onOpenChange={handleOpenChange}>
+            <Dialog.Portal>
+                <Dialog.Overlay className={styles.DialogManager__Overlay} />
+                <Dialog.Content
+                    aria-describedby={undefined}
+                    className={styles.DialogManager__Content}
+                >
+                    <Dialog.Title className={styles.DialogManager__Title}>Dialog</Dialog.Title>
+                    {children}
+                </Dialog.Content>
+            </Dialog.Portal>
+        </Dialog.Root>
+    )
+}
