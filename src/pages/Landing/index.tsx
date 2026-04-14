e6286d5a9977316f711c5607408737d666e96422
Juno Nguyen <juno.ngx@gmail.com>
2026-04-14 23:03:05 +0800

    feat: implement landing page

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,3 +1,15 @@
+import { Link } from "react-router-dom"
+import styles from "./index.module.scss"
+
 export default function Landing() {
-    return <div>Landing</div>
+    return (
+        <div className={styles.Landing}>
+            <h1 className={styles.Landing__Title}>ai*jot</h1>
+            <p className={styles.Landing__Tagline}>*sloth, not LLM</p>
+            <p className={styles.Landing__Tagline}>A minimal keyboard-first note app</p>
+            <Link to="/jot" className={styles.Landing__Btn}>
+                Get started
+            </Link>
+        </div>
+    )
 }
