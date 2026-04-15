d41d08aae5532bd80e9c3835d4661093cb0bb053
Juno Nguyen <juno.ngx@gmail.com>
2026-04-15 23:51:18 +0800

    ui: implement layout vertical padding
    To prevent header from blocking content

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Navigate, Outlet } from "react-router-dom"
+import { Routes, Route, Navigate } from "react-router-dom"
 import Landing from "@/pages/Landing"
 import Jot from "@/pages/Jot"
 import Collections from "@/pages/Collections"
@@ -6,7 +6,7 @@ import Settings from "@/pages/Settings"
 import Help from "@/pages/Help"
 import Privacy from "@/pages/Privacy"
 import Terms from "@/pages/Terms"
-import Header from "@/components/Header"
+import AppLayout from "@/components/AppLayout"
 import { useProfileSettings } from "@/store/profileSettings"
 import {
     ROUTE_JOT,
@@ -17,15 +17,6 @@ import {
     ROUTE_PRIVACY,
     ROUTE_TERMS,
 } from "@/utils/constants"
-
-function AppLayout() {
-    return (
-        <>
-            <Header />
-            <Outlet />
-        </>
-    )
-}
 
 function JotRedirect() {
     const defaultCollectionSlug = useProfileSettings(
