1a7236d1ea894ff1a48ad42f2da505da2cb27e32
Juno Nguyen <juno.ngx@gmail.com>
2026-04-18 00:02:05 +0800

    #1 ui: replace theme mode dropdown with theme button on header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,14 @@
 import { Link, useMatch } from "react-router-dom"
 import UserDropdown from "./UserDropdown"
-import ThemeModeDropdown from "./ThemeModeDropdown"
 import CollectionDropdown from "./CollectionDropdown"
 import styles from "./Header.module.scss"
-import { ROUTE_JOT, ROUTE_COLLECTION } from "@/utils/constants"
+import {
+    ROUTE_JOT,
+    ROUTE_COLLECTION,
+    ICON_PROPS_HEADER,
+} from "@/utils/constants"
+import { IconPalette } from "@tabler/icons-react"
+import { useCommandPaletteStore } from "@/store/commandPaletteStore"
 
 export default function Header() {
     const isJotRoute = useMatch(ROUTE_COLLECTION)
@@ -32,7 +37,14 @@ export default function Header() {
                 <div
                     className={`${styles.Header__Block} ${styles["Header__Block--Right"]}`}
                 >
-                    <ThemeModeDropdown />
+                    <button
+                        className={styles.Header__BtnTheme}
+                        onClick={() => {
+                            useCommandPaletteStore.getState().open("theme")
+                        }}
+                    >
+                        <IconPalette {...ICON_PROPS_HEADER} />
+                    </button>
                     <UserDropdown />
                 </div>
             </div>
