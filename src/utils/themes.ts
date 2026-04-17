019c44421b441f1cda3adb9562f6e7fd1e250493
Juno Nguyen <juno.ngx@gmail.com>
2026-04-17 22:29:20 +0800

    #1 dev: add themes.ts with light and dark theme definitions

---
+++ src/utils/themes.ts
@@ -0,0 +1,24 @@
+export const themes = [
+    {
+        name: "light",
+        colBg: "#e0e0e0",
+        colMain: "#0098a9",
+        colSub: "#777777",
+        colBgSub: "#cfcfcf",
+        colText: "#121212",
+        colDanger: "#fa5252",
+    },
+    {
+        name: "dark",
+        colBg: "#121212",
+        colMain: "#0098a9",
+        colSub: "#777777",
+        colBgSub: "#333333",
+        colText: "#e0e0e0",
+        colDanger: "#fa5252",
+    },
+] as const
+
+export type ThemeName = (typeof themes)[number]["name"]
+
+export type ThemeColors = Omit<(typeof themes)[number], "name">
