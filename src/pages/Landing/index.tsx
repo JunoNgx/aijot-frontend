d5be65b3df6dd499e797ba7e7278599324a6b0f6
Juno Nguyen <juno.ngx@gmail.com>
2026-04-24 01:51:47 +0800

    feat: tweak landing page

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -24,30 +24,50 @@ export default function Landing() {
             </section>
 
             <section className={styles.Landing__Section}>
-                <h2 className={styles.Landing__SectionTitle}>Use keyboard; be more like sloth</h2>
+                <h2 className={styles.Landing__SectionTitle}>
+                    Use keyboard; be more like sloth
+                </h2>
                 <p className={styles.Landing__Text}>
-                    Sloth achieves a lot despite moving very little, just like you can with your keyboard. Create everything with tags from one single input, and do everything else with command palette like a neovim nerd, because you can in this app.
+                    Sloth achieves a lot despite moving very little, just like
+                    you can with your keyboard. Create everything with tags from
+                    one single input, and do everything else with command
+                    palette like a neovim nerd, because you can in this app.
                 </p>
             </section>
 
             <section className={styles.Landing__Section}>
-                <h2 className={styles.Landing__SectionTitle}>A simple, sane, and flexible system</h2>
+                <h2 className={styles.Landing__SectionTitle}>
+                    A simple, sane, and flexible system
+                </h2>
                 <p className={styles.Landing__Text}>
-                    All you need for noting: text, bookmarks, and todos. Add tags to create collections. Browse what you want to see, and instantly search for what you need.
+                    All you need for noting: text, bookmarks, and todos. Add
+                    tags to create collections. Browse what you want to see, and
+                    instantly search for what you need.
                 </p>
             </section>
 
             <section className={styles.Landing__Section}>
-                <h2 className={styles.Landing__SectionTitle}>Dark/light theme is so 2015; get some personality</h2>
+                <h2 className={styles.Landing__SectionTitle}>
+                    Dark/light theme is so 2015; get some personality
+                </h2>
                 <p className={styles.Landing__Text}>
-                    Spotify can't even do light theme as of 2026, but I don't work for them. Choose from original tasteful themes, or 60 other well-known and proven colour palettes. Randomise or pick one. Go wild.
+                    Spotify can't even do light theme as of 2026, but I don't
+                    work for them. Choose from original tasteful themes, or 60
+                    other well-known and proven colour palettes. Randomise or
+                    pick one. Go wild.
                 </p>
             </section>
 
             <section className={styles.Landing__Section}>
-                <h2 className={styles.Landing__SectionTitle}>All the FOSS fuss</h2>
+                <h2 className={styles.Landing__SectionTitle}>
+                    All the FOSS fuss
+                </h2>
                 <p className={styles.Landing__Text}>
-                    You probably know the drill. No ad, no sub, no tracking. Open source and free forever. Fork and host all you want. Backend only does what it needs, no remote database. Data only leave your device to go to your Google Drive, only when you choose.
+                    You probably know the drill. No ad, no sub, no tracking.
+                    Open source and free forever. Fork and host all you want.
+                    Backend only does what it needs, no remote database. Data
+                    only leave your device to go to your Google Drive, only when
+                    you choose.
                 </p>
             </section>
 
